82f0f46b6940222b07644648e2f23ff8ccb2a877
Vardhan Shivandhi <shivandhivardhan01@gmail.com>
2026-06-04 11:55:49 -0700

    homepage built -greeting , search bar ,background

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Create the Flask app
 app = Flask(__name__)
 
-# Load the API key
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
-# Test route - just to confirm the app is running
 @app.route("/")
 def home():
-    return "Haarv Tech is live!"
+    return render_template("index.html")
 
-# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
